9bd2beb86e00ac29cd58d7ec11de8a288209694e
Naveen Patel <newnaveen573@gmail.com>
2025-11-13 23:16:23 +0530

    added eureka auth and api gatway services

---
+++ ExoCommerce-Backend/eureka-service/src/test/java/com/exocommmerce/eureka_service/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exocommmerce.eureka_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
