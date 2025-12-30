78afc79c4545b49cb5111433f592e67c8a76efa3
Naveen7222 <newnaveen573@gmail.com>
2025-12-30 19:13:12 +0530

    dsfvd

--- order-service/src/main/java/com/exocommerce/order_service/config/SecurityConfig.java
+++ order-service/src/main/java/com/exocommerce/order_service/config/SecurityConfig.java
@@ -1,32 +1,43 @@
 package com.exocommerce.order_service.config;
 
+import javax.crypto.spec.SecretKeySpec;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+    private static final String SECRET =
+            "EXO_SECRET_KEY_123_EXO_SECRET_KEY_123";
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                // We are a stateless REST API
                 .csrf(csrf -> csrf.disable())
-
-                // Authorize requests
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
-
-                // Enable JWT authentication
                 .oauth2ResourceServer(oauth2 ->
                         oauth2.jwt(Customizer.withDefaults())
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+
+        SecretKeySpec secretKey =
+                new SecretKeySpec(SECRET.getBytes(), "HmacSHA256");
+
+        return NimbusJwtDecoder.withSecretKey(secretKey).build();
     }
 }
